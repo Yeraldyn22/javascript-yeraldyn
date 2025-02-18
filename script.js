7bfa112a3ca17fd6820e84268cac51b4497d9db0
yeraldyn <yeraldinaros48@gmail.com>
2025-02-18 09:54:57 -0500

    ejercicio 1y2 de javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+
+//ejercicio 1
+let nombre = "yeraldyn aros" ;
+let edad = 29; 
+let ciudad = "Garzon";
+console.log("hola, mi nombres es " + nombre + ",tengo"+ edad + "años y vivo en" + ciudad + ".");
+
+//ejercicio 2 
+/*crea una variable llamada precio y asignale un valor decimal . luego crea otra variable llamada 
+cantidad  y asignale un valor entero . muestra el tipo de variable en la consola
+*/
+
+let precio = 2.25;
+let cantidad = 3; 
+
+//escribir en una consola 
+console.log(typeof precio);
+console.log(typeof cantidad);
+//escribir una alerta 
+alert(typeof precio);
+//escribir en un documento
+document.write(typeof cantidad);
